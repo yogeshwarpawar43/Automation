f80e059077fc909e4cc2efc2b38b3619e30ce097
Yogeshwar Pawar <yogeshwarpawar43@gmail.com>
2025-10-10 08:12:30 +0530

    Learning git

---
+++ src/main/java/com/selenium/base/BaseClass.java
@@ -0,0 +1,21 @@
+package com.selenium.base;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class BaseClass {
+	
+	WebDriver driver;
+	
+	@Test
+	public void setup() {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\yoges\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		//System.setProperty("webdriver.chrome.driver", "C:\Users\yoges\eclipse-workspace\learn.selenium.com\src\test\resources\driversdriverschromedriver.exe");
+		//driver = new ChromeDriver();
+		driver.get("https://www.google.com");
+		driver.manage().window().maximize();
+	}
+
+}

7bec1cad14e39e82832b28e1278cd75e97996bd2
Yogeshwar Pawar <yogeshwarpawar43@gmail.com>
2025-11-06 23:33:00 +0530

    Correct files

--- src/main/java/com/selenium/base/BaseClass.java
+++ src/main/java/com/selenium/base/BaseClass.java
@@ -5,45 +5,21 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 
-import com.selenium.utiles.ConfigReader;
-
 public class BaseClass {
 
 	protected WebDriver driver;
-	protected static ConfigReader config;
-	protected static ConfigReader testData;
-
-	static {
-        try {
-	   String projectPath = System.getProperty("user.dir");
-
-       // Build relative paths to config files
-       String configPath = projectPath + "/src/test/resources/config/config.properties";
-       String testDataPath = projectPath + "/src/test/resources/config/locators.properties";
-
-       // Load both property files
-       config = new ConfigReader(configPath);
-       testData = new ConfigReader(testDataPath);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
+	
 	
 	@BeforeSuite
 	public void setUp() {
 		
 		
-     
-		String browser = config.getProperty("browser");
-		String url = config.getProperty("url");
-
-		if (browser.equalsIgnoreCase("chrome")) {
+    
+	
 			driver = new ChromeDriver();
-		}
-
+		
 		driver.manage().window().maximize();
-		driver.get(url);
+		driver.get("https:www.google.com");
 	}
 
 	@AfterSuite
